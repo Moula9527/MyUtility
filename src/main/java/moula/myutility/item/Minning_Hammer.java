662957c32caa93654b24b9da3bda915eb5ca3d3a
Moula9527 <33978882+Moula9527@users.noreply.github.com>
2021-03-15 23:42:03 +0800

    add Scythe texture

---
+++ src/main/java/moula/myutility/item/Minning_Hammer.java
@@ -0,0 +1,26 @@
+package moula.myutility.item;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.RaycastContext;
+import net.minecraft.world.World;
+
+public class Minning_Hammer extends PickaxeItem {
+
+    protected Minning_Hammer(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
+        HitResult hitResult = raycast(world,(PlayerEntity) miner, RaycastContext.FluidHandling.NONE);
+
+        return super.postMine(stack, world, state, pos, miner);
+    }
+}

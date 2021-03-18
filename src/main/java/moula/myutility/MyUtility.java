849cec6ee2d97e3f46ca9d878462da4ef37996ac
Moula9527 <33978882+Moula9527@users.noreply.github.com>
2021-03-18 21:28:56 +0800

    add Minning_Hammer feature

--- src/main/java/moula/myutility/MyUtility.java
+++ src/main/java/moula/myutility/MyUtility.java
@@ -3,6 +3,7 @@ package moula.myutility;
 import moula.myutility.block.Pedestal;
 import moula.myutility.block.entity.Pedestal_Entity;
 import moula.myutility.item.Animal_sack;
+import moula.myutility.item.Minning_Hammer;
 import moula.myutility.item.Scythe;
 import moula.myutility.item.Time_Cloth;
 import net.fabricmc.api.ModInitializer;
@@ -25,6 +26,7 @@ public class MyUtility implements ModInitializer {
     public static final Item Animal_sack = new Animal_sack(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1));
     public static final Item Time_cloth = new Time_Cloth(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1));
     public static final Item Scythe = new Scythe(ToolMaterials.IRON, 3, -2.4F, new FabricItemSettings().group(ItemGroup.MISC),3);
+    public static final Item Minning_Hammer = new Minning_Hammer(ToolMaterials.DIAMOND,9,-3.5F,new FabricItemSettings().group(ItemGroup.MISC));
     public static final Block Pedestal = new Pedestal(FabricBlockSettings.of(Material.METAL).strength(4.0f));
     public static BlockEntityType<Pedestal_Entity> PedestalEntity;
 
@@ -33,6 +35,7 @@ public class MyUtility implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier("myutility","animal_sack"), Animal_sack);
         Registry.register(Registry.ITEM,new Identifier("myutility","time_cloth"),Time_cloth);
         Registry.register(Registry.ITEM,new Identifier("myutility","scythe"),Scythe);
+        Registry.register(Registry.ITEM,new Identifier("myutility","minninghammer"),Minning_Hammer);
         Registry.register(Registry.BLOCK,new Identifier("myutility","pedestal"),Pedestal);
         Registry.register(Registry.ITEM,new Identifier("myutility","pedestal"),new BlockItem(Pedestal,new FabricItemSettings().group(ItemGroup.MISC)));
         PedestalEntity = Registry.register(Registry.BLOCK_ENTITY_TYPE,"myutility:pedestalentity",BlockEntityType.Builder.create(Pedestal_Entity::new, Pedestal).build(null));
--- src/main/java/moula/myutility/item/Minning_Hammer.java
+++ src/main/java/moula/myutility/item/Minning_Hammer.java
@@ -1,26 +1,62 @@
 package moula.myutility.item;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 
 public class Minning_Hammer extends PickaxeItem {
 
-    protected Minning_Hammer(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+    public Minning_Hammer(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
     }
 
     @Override
     public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
+        world.setBlockState(pos, state);
         HitResult hitResult = raycast(world,(PlayerEntity) miner, RaycastContext.FluidHandling.NONE);
-
+        Direction direction;
+        direction = ((BlockHitResult)hitResult).getSide();
+        int mx = 1;
+        int my = 1;
+        int mz = 1;
+        switch (direction.getAxis()){
+            case X:
+                mx = 0;
+                break;
+            case Y:
+                my = 0;
+                break;
+            case Z:
+                mz = 0;
+                break;
+        }
+        int[] var = {-1,0,1};
+        for (int x:var){
+            for (int y:var){
+                for (int z:var){
+                    BlockPos newpos = pos.add((x*mx),(y*my),(z*mz));
+                    BlockState blockState = world.getBlockState(newpos);
+                    if (blockState.getBlock() == Blocks.STONE){
+                        blockState.getBlock().onBreak(world, newpos, blockState, (PlayerEntity) miner);
+                        boolean bl = world.breakBlock(newpos,!newpos.equals(pos),null,3);
+                        if (bl) {
+                            blockState.getBlock().onBroken(world, newpos, blockState);
+                        }
+                    }
+                }
+            }
+        }
         return super.postMine(stack, world, state, pos, miner);
     }
 }
